47c51f6edb6529f5b90a21d8bbac4c77c9103d86
Dennis Mugambi <dennis.mutwiri.mugambi@gmail.com>
2019-04-13 12:15:50 +0300

    add all folder files

---
+++ Assets/Game/Materials/Scripts/player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 17.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Current position = new position;
+        //transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+    }
+    private void movement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+        transform.Translate(Vector3.up * speed * verticalInput * Time.deltaTime);
+
+        if (transform.position.y < -4)
+        {
+            transform.position = new Vector3(transform.position.x, -4, 0);
+        }
+        else if (transform.position.y > 4)
+        {
+            transform.position = new Vector3(transform.position.x, 4, 0);
+        }
+
+        if (transform.position.x > 8)
+        {
+            transform.position = new Vector3(-8, transform.position.y, 0);
+        }
+        else if (transform.position.x < -8)
+        {
+            transform.position = new Vector3(8, transform.position.y, 0);
+        }
+    }
+}
